d7fa45bb648acb874e57027b4603d7bbec0af5d5
Medfakhir <mohamed.fakhirfakhor@gmail.com>
2023-02-18 13:22:36 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Mohamed Fakhir Fakhir
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d \n", number);
+
+
+
+	return (0);
+i}
